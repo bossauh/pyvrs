ced9d251d2c76cf3c55aaddbbcc6ffe9682bd02a
Philippe Mathew <philmattdev@gmail.com>
2022-10-28 11:50:01 +0800

    fix: dependency issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 install_requires = [
     "tensorflow==2.10.0",
-    "keras==2.6.0",
+    "keras==2.10.0",
     "numpy",
     "nest_asyncio",
     "maglevapi",
@@ -19,13 +19,13 @@ install_requires = [
 setup(
     name="pyvrs",
     packages=["advanced_vrs"],
-    version="0.7",
+    version="0.8",
     license="MIT",
     description="A powerful voice recognition library made in python. This combines several libraries in order to achieve a 'Voice Assistant' ready library.",
     author="Philippe Mathew",
     author_email="philmattdev@gmail.com",
     url="https://github.com/bossauh/pyvrs",
-    download_url="https://github.com/bossauh/pyvrs/archive/refs/tags/v_07.tar.gz",
+    download_url="https://github.com/bossauh/pyvrs/archive/refs/tags/v_08.tar.gz",
     keywords=["voice", "speech to text", "voice recognition"],
     install_requires=install_requires
 )
